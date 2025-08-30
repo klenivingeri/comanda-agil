0b06fbf1c27724cc410273fd1aab25ce0ec2e482
klenivingeri <libertti@live.com>
2025-08-30 19:51:49 -0300

    fix: get params

--- src/app/atendimento/page.js
+++ src/app/atendimento/page.js
@@ -1,6 +1,6 @@
 "use client";
-
-import React, { useEffect, useState, useMemo } from "react";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState, useMemo, Suspense } from "react";
 import { InputSearch } from "../../components/input/inputSearch";
 import { Container } from "../../components/layout/Container";
 import { Content } from "../../components/layout/Content";
@@ -14,7 +14,7 @@ import { IconDotMenu } from "../../../public/icons/DotMenu";
 import { IconMenuSquare } from "../../../public/icons/MenuSquare";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+
 import { isEmpty } from "../utils/empty";
 import { MenuMobile } from "../../components/menu/lateral/MenuMobile";
 
@@ -95,106 +95,108 @@ export default function Atendimento() {
   }, [comanda?.items, itemsSelected]);
 
   return (
-    <Container>
-      <Header>
-        <div className="flex flex-col w-full gap-2 justify-start">
-          <div className="w-full grid grid-cols-12 px-2 h-[40px]">
-            <Link
-              href="/comandas"
-              className="col-span-2 flex items-center pt-2 gap-1"
-            >
-              <IconMenuSquare size="h-[26px] w-[26px]" />
-              <div className="font-bold text-lg text-black">123</div>
-            </Link>
-            <div className="col-span-8 flex items-center">
-              <div className="w-full flex justify-center">
-                <span className="text-md font-bold">Cardapio</span>
+    <Suspense>
+      <Container>
+        <Header>
+          <div className="flex flex-col w-full gap-2 justify-start">
+            <div className="w-full grid grid-cols-12 px-2 h-[40px]">
+              <Link
+                href="/comandas"
+                className="col-span-2 flex items-center pt-2 gap-1"
+              >
+                <IconMenuSquare size="h-[26px] w-[26px]" />
+                <div className="font-bold text-lg text-black">123</div>
+              </Link>
+              <div className="col-span-8 flex items-center">
+                <div className="w-full flex justify-center">
+                  <span className="text-md font-bold">Cardapio</span>
+                </div>
+              </div>
+              <div
+                className="col-span-2 flex items-end justify-end pt-1"
+                onClick={handleOpenMenuMobile}
+              >
+                <IconDotMenu size="h-[32px] w-[32px]" />
               </div>
             </div>
-            <div
-              className="col-span-2 flex items-end justify-end pt-1"
-              onClick={handleOpenMenuMobile}
-            >
-              <IconDotMenu size="h-[32px] w-[32px]" />
+            <div className="w-full grid grid-cols-12 px-2 gap-2">
+              <div className="col-span-12 flex items-center gap-2">
+                <InputSearch setInputText={setInputText} />
+                <button
+                  onClick={handleOpenModal}
+                  className="relative text-black rounded-md flex justify-center items-center h-[40px] px-2"
+                >
+                  <span>
+                    <IconMenuList size="h-[32px] w-[32px]" />
+                  </span>
+                  {!!totalItems.length && (
+                    <div className="absolute text-xs text-white h-4 w-4 bg-[var(--button)] top-[-1px] right-[10px] rounded-full flex justify-center items-center leading-none">
+                      {totalItems.length}
+                    </div>
+                  )}
+                </button>
+              </div>
             </div>
           </div>
-          <div className="w-full grid grid-cols-12 px-2 gap-2">
-            <div className="col-span-12 flex items-center gap-2">
-              <InputSearch setInputText={setInputText} />
-              <button
-                onClick={handleOpenModal}
-                className="relative text-black rounded-md flex justify-center items-center h-[40px] px-2"
-              >
-                <span>
-                  <IconMenuList size="h-[32px] w-[32px]" />
-                </span>
-                {!!totalItems.length && (
-                  <div className="absolute text-xs text-white h-4 w-4 bg-[var(--button)] top-[-1px] right-[10px] rounded-full flex justify-center items-center leading-none">
-                    {totalItems.length}
-                  </div>
-                )}
+        </Header>
+        <div className="mt-[85px] mb-[50px] flex-1 flex flex-col">
+          <Content isLoading={isLoading}>
+            {isEmpty(idComanda) ? (
+              <div>Selecione uma comanda antes de adiconar os produtos</div>
+            ) : (
+              <ItemList
+                items={items}
+                inputText={inputText}
+                handleUpdateItemsSelected={handleUpdateItemsSelected}
+              />
+            )}
+          </Content>
+        </div>
+        <Footer>
+          <div className="flex justify-center items-center w-full">
+            <div className="relative w-full flex justify-center items-center">
+              <button className="text-white shadow-sm font-bold py-3 px-4 rounded-3xl w-full m-2 bg-[var(--button)] hover:bg-[var(--buttonHover)]">
+                <span className=""> LANÇAR ITEMS NA COMANDA</span>
               </button>
             </div>
           </div>
-        </div>
-      </Header>
-      <div className="mt-[85px] mb-[50px] flex-1 flex flex-col">
-        <Content isLoading={isLoading}>
-          {isEmpty(idComanda) ? (
-            <div>Selecione uma comanda antes de adiconar os produtos</div>
-          ) : (
-            <ItemList
-              items={items}
-              inputText={inputText}
-              handleUpdateItemsSelected={handleUpdateItemsSelected}
-            />
-          )}
-        </Content>
-      </div>
-      <Footer>
-        <div className="flex justify-center items-center w-full">
-          <div className="relative w-full flex justify-center items-center">
-            <button className="text-white shadow-sm font-bold py-3 px-4 rounded-3xl w-full m-2 bg-[var(--button)] hover:bg-[var(--buttonHover)]">
-              <span className=""> LANÇAR ITEMS NA COMANDA</span>
-            </button>
-          </div>
-        </div>
-      </Footer>
+        </Footer>
 
-      <ModalRight
-        handleOpenModal={handleOpenModal}
-        openModal={openModal}
-        totalComanda={totalComanda}
-      >
-        <div>
-          {comanda?.items?.map((item, idx) => (
-            <Item
-              key={idx}
-              item={item}
-              handleAddTotalItemsInTheCategiry={() => {}}
-              handleRemoveTotalItemsInTheCategiry={() => {}}
-              handleUpdateItemsSelected={handleUpdateItemsSelected}
-              hiddeSelectQuantity
-              itemInNote
-            />
-          ))}
-          {itemsSelected.map((item, idx) => (
-            <Item
-              key={idx}
-              item={item}
-              handleAddTotalItemsInTheCategiry={() => {}}
-              handleRemoveTotalItemsInTheCategiry={() => {}}
-              handleUpdateItemsSelected={handleUpdateItemsSelected}
-              itemInNote
-              hideValue
-            />
-          ))}
-        </div>
-      </ModalRight>
-      <MenuMobile
-        handleOpenModal={handleOpenMenuMobile}
-        openModal={openMenuMobile}
-      />
-    </Container>
+        <ModalRight
+          handleOpenModal={handleOpenModal}
+          openModal={openModal}
+          totalComanda={totalComanda}
+        >
+          <div>
+            {comanda?.items?.map((item, idx) => (
+              <Item
+                key={idx}
+                item={item}
+                handleAddTotalItemsInTheCategiry={() => {}}
+                handleRemoveTotalItemsInTheCategiry={() => {}}
+                handleUpdateItemsSelected={handleUpdateItemsSelected}
+                hiddeSelectQuantity
+                itemInNote
+              />
+            ))}
+            {itemsSelected.map((item, idx) => (
+              <Item
+                key={idx}
+                item={item}
+                handleAddTotalItemsInTheCategiry={() => {}}
+                handleRemoveTotalItemsInTheCategiry={() => {}}
+                handleUpdateItemsSelected={handleUpdateItemsSelected}
+                itemInNote
+                hideValue
+              />
+            ))}
+          </div>
+        </ModalRight>
+        <MenuMobile
+          handleOpenModal={handleOpenMenuMobile}
+          openModal={openMenuMobile}
+        />
+      </Container>
+    </Suspense>
   );
 }
