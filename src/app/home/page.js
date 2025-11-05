cc5eb1766720c253197a13833913ec54336674ad
klenivingeri <libertti@live.com>
2025-11-04 23:57:59 -0300

    chore: adicinado nome na pagina inicial

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,14 +5,17 @@ import { ButtonContainer } from "src/components/button";
 import { Container } from "src/components/layout/Container";
 import { Content } from "src/components/layout/Content";
 import { Header } from "src/components/layout/Header";
+import { useUserConfig } from "src/app/context/UserContext";
 
 export default function Empresa() {
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
+  const { _user } = useUserConfig();
+
   return (
     <Container>
-      <Header divider menu title="Bem-vindo, Fulano" />
+      <Header divider menu title={`Bem-vindo, ${_user.all[0]?.name}`} />
       <Content isLoading={isLoading} error={error}>
         <ButtonContainer href="/comandas" hFull="h-22" margin="mt-1">
           <span className="pl-1 flex items-center gap-2">
